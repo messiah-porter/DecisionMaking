3cf36f86321fe3b2ca06aaea2627f632036cce84
messiah-porter <messiah.porter@stu.barren.kyschools.us>
2025-02-06 11:01:04 -0600

    bonus done

--- work.js
+++ work.js
@@ -30,3 +30,27 @@ if (randomNum == 2 || randomNum == 4) {
   console.log('equal to 2 OR 4');
   
 }
+
+// if (randomNum >= 4 ? 'equal to or more then 4' : 'less then 4' ) 
+console.log(randomNum >=4 ? 'equal to or more then 4' : 'less then 4');
+
+switch (randomNum) {
+  case randomNum = 0:
+    console.log('zero');
+    break
+  case randomNum = 1:
+    console.log("one");
+    break
+ case randomNum = 2:
+    console.log("two");
+    break
+  case randomNum= 3:
+    console.log('three');
+    break
+  case randomNum = 4:
+    console.log('four');
+    break
+  case randomNum = 5:
+    console.log('five');
+    break
+}
